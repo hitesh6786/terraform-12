15ffd70626ed2e4f4a7a865d259e02e16fd5cfd5
Hitesh Mittal <hiteshmittal6786@gmail.com>
2024-05-28 11:03:36 +0530

    terraform cloud-testing
    terraform cloud day 12

---
+++ file.tf
@@ -0,0 +1,77 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform-bucket-27-5-2024"
+        key = "hitesh-terraform/terraform.tfstate"
+        region = "us-east-1"
+        profile = "Hitesh"
+       
+
+      
+    }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "Hitesh"
+  region  = "us-east-1"
+  # Configuration options
+}
+
+
+
+
+#instance
+
+
+resource "aws_instance" "Public-ec2" {
+  ami           = data.aws_ami.amazonlinux.id
+  instance_type = "t2.micro"
+  count=1
+  key_name = "terraform-keypair"
+
+  
+  
+tags = {
+    Name = "Public-ec2-1"
+  }
+}
+
+
+#instance
+
+
+#data source
+
+data "aws_ami" "amazonlinux" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+}
+
+  filter {
+    name = "architecture"
+    values= ["x86_64"]
+  }
+
+}
+
+
